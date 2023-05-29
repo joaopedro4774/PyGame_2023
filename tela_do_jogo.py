52f94927de5c87a1e45dd123c73b0f5707625642
Felipe Assed <felipeac5@al.insper.edu.br>
2023-05-29 16:10:36 -0300

    tela_do_jogo

---
+++ tela_do_jogo.py
@@ -0,0 +1,16 @@
+import pygame, random
+from pygame.locals import *
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 800
+SPEED = 10
+GRAVITY = 1
+GAME_SPEED = 10
+
+GROUND_WIDTH = 2 * SCREEN_WIDTH
+GROUND_HEIGHT = 100
+
+PIPE_WIDTH = 80
+PIPE_HEIGHT = 500
+
+PIPE_GAP = 200
